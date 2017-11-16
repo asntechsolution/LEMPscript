f0a6615ba45489af1b68ae4c1a24aa8014ecdea4
asntechsolution <admin@serversupport24x7.com>
2017-11-17 02:29:53 +0530

    Update lempsetup.sh

--- lempsetup.sh
+++ lempsetup.sh
@@ -35,11 +35,54 @@ echo Please enter your host/domain for fresh installation only and press Enter.
 echo if this is not fresh installation then press CTRL+c or the current system will be destroyed.
 read -r mys
 
+echo 'server {
+
+        root /var/www/html;
+        index index.php index.html index.htm index.nginx-debian.html;
+
+        server_name _;
+        gzip on;
+        location / {
+                try_files $uri $uri/ =404;
+        }
+
+location ~ \.php$ {
+                include snippets/fastcgi-php.conf;
+                fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+        }
+
+
+}' > tmp
+
+mv tmp /etc/nginx/sites-available/$mys
+ln -sf /etc/nginx/sites-available/$mys /etc/nginx/sites-enabled/$mys
+mkdir -p /var/www/$mys
+checkerr mkdir -p /var/www/$mys
+sed -i "s/\/var\/www\/html/\/var\/www\/$mys/g"  /etc/nginx/sites-available/$mys
+sed -i "s/server_name\ _/server_name\ $mys www.$mys/g"  /etc/nginx/sites-available/$mys
+
+certbot --nginx -d "$mys" -d "www.$mys" --register-unsafely-without-email --agree-tos -n # can use -m for email
+checkerr certbot --nginx -d "$mys" -d "www.$mys" --register-unsafely-without-email --agree-tos -n
+nginx -t
+checkerr
+
+systemctl reload nginx
+checkerr
+
+cd /var/www/$mys/
+wget https://files.phpmyadmin.net/phpMyAdmin/4.7.5/phpMyAdmin-4.7.5-all-languages.tar.gz
+tar -xzf phpMyAdmin-4.7.5-all-languages.tar.gz
+rm -rf phpMyAdmin-4.7.5-all-languages.tar.gz
+mv phpMyAdmin-4.7.5-all-languages/ phpmyadmin
+cp /var/www/html/index.nginx-debian.html /var/www/$mys/
+#chown -R ftpman:www-data /var/www/
+
 ## Check root and os
 
 apt-get update ; apt-get -y install nginx php-curl curl mysql-server
 
 useradd -d /var/www/html/ -G www-data ftpman
+chown -R ftpman:www-data /var/www/
 checkerr
 echo "ftpman:$mys" | chpasswd
 checkerr echo "ftpman:$mys" '| chpasswd'
@@ -50,7 +93,7 @@ add-apt-repository ppa:certbot/certbot -y
 checkerr add-apt-repository ppa:certbot/certbot -y
 apt-get update
 apt-get install python-certbot-nginx -y
-sed -i "s/server_name\ _/server_name\ $mys/g"  /etc/nginx/sites-available/default
+#sed -i "s/server_name\ _/server_name\ $mys/g"  /etc/nginx/sites-available/default
 ##check nginx syntax
 nginx -t
 checkerr nginx -t
@@ -60,8 +103,8 @@ checkerr
 ufw allow 'Nginx Full'
 #ufw delete allow 'Nginx HTTP'
 
-certbot --nginx -d "$mys" --register-unsafely-without-email --agree-tos -n # can use -m for email
-checkerr certbot --nginx -d "$mys" --register-unsafely-without-email --agree-tos -n
+#certbot --nginx -d "$mys" --register-unsafely-without-email --agree-tos -n # can use -m for email
+#checkerr certbot --nginx -d "$mys" --register-unsafely-without-email --agree-tos -n
 
 sb=`grep -n 'server {' /etc/nginx/sites-available/default | grep -v '#' | head -n 1 | cut -f1 -d":"`
 sb=`expr $sb + 3`
@@ -132,4 +175,3 @@ echo "CONGRATULATIONS ! INSTALLATION IS READY, YOU MAY LOGIN TO SFTP SERVICE"
 echo "##### USING USERNAME : ftpman , AND PASSWORD : $mys ..    ############"
 echo "##### Port : 22 ######## Software recommended : Filezilla ############"
 echo "######################################################################"
-
